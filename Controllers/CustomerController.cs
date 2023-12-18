db03e5c9c09efde8ec3c86cf0e33bab38e3133a1
Visitor <nullemail@qq.com>
2023-12-18 23:56:59 +0800

    add ValidateInput method when update customer's score

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -12,8 +12,34 @@ namespace CustomerLeaderboard.Controllers
         [HttpPost("{customerid}/score/{score}")]
         public async Task<ActionResult<decimal>> UpdateScore(long customerId, decimal score)
         {
+            var validationErrors = ValidateInput(customerId, score);
+            if (validationErrors.Count != 0)
+            {
+                foreach (var error in validationErrors)
+                {
+                    ModelState.AddModelError(error.Key, error.Value);
+                }
+                return BadRequest(ModelState);
+            }
+
             var updatedScore = await customerRankingService.UpdateScore(customerId, score);
             return updatedScore;
+        }
+
+        private static Dictionary<string, string> ValidateInput(long customerId, decimal score)
+        {
+            var errors = new Dictionary<string, string>();
+
+            if (customerId <= 0)
+            {
+                errors.Add("customerId", "CustomerId must be a positive number.");
+            }
+            if (score < -1000 || score > 1000)
+            {
+                errors.Add("score", "Score must be in the range [-1000, 1000].");
+            }
+
+            return errors;
         }
     }
 }
