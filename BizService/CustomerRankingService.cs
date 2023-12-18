96ce16cf0e221665235e70605f01182d0696edf3
Visitor <nullemail@qq.com>
2023-12-18 18:11:32 +0800

    update the GetCustomersByCustomerId method to fix bug: 1.skip matched customer when build the collection of Neighbors 2.update the way to calculate the "Rank" of customer

--- BizService/CustomerRankingService.cs
+++ BizService/CustomerRankingService.cs
@@ -41,26 +41,30 @@ namespace CustomerLeaderboard.BizService
                 {
                     return CustomerWithNeighbors.EMPTY;
                 }
-                var customer = new Customer { CustomerID = customerId, Score = scores[customerId] };
+
+                decimal customerScore = scores[customerId];
+
                 var leaderboard = scores
                     .OrderByDescending(entry => entry.Value)
                     .ThenBy(entry => entry.Key)
-                    .Select((entry, index) => new Customer { CustomerID = entry.Key, Score = entry.Value, Rank = index + 1 })
+                    .Select((entry, index) => new Customer
+                    {
+                        CustomerID = entry.Key,
+                        Score = entry.Value,
+                        Rank = index + 1
+                    })
                     .ToList();
 
-                var startIndex = Math.Max(0, leaderboard.FindIndex(c => c.CustomerID == customerId) - high);
-                var endIndex = Math.Min(leaderboard.Count - 1, leaderboard.FindIndex(c => c.CustomerID == customerId) + low);
+                var customer = leaderboard.Find(cust => cust.CustomerID == customerId);
+                int customerRank = customer?.Rank ?? 0;
 
                 var customers = leaderboard
-                    .Skip(startIndex)
-                    .Take(endIndex - startIndex + 1)
-                    .ToList();
+                .Where(cust => cust.Rank >= customerRank - high && cust.Rank <= customerRank + low && cust.CustomerID != customerId)
+                .ToList();
 
                 var result = new CustomerWithNeighbors { Customer = customer, Neighbors = customers };
-
                 return result;
-            }
-                );
+            });
         }
     }
 }
